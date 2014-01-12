bebbc6d7fccdf1d3128aa9ca8c28907077a33371
Paul S. Nomura <paul.s.nomura@gmail.com>
2014-01-13 04:40:01 +0900

    Make TableDataServer much simpler

--- mdserver/src/main/scala/com/paulsnomura/mdserver/table/TableDataServer.scala
+++ mdserver/src/main/scala/com/paulsnomura/mdserver/table/TableDataServer.scala
@@ -1,16 +1,15 @@
 package com.paulsnomura.mdserver.table
 
 import org.apache.logging.log4j.LogManager
-
 import com.paulsnomura.mdserver.Publisher
 import com.paulsnomura.mdserver.Subscriber
+import com.paulsnomura.mdserver.table.TableDataServer.ClientStartup
 import com.paulsnomura.mdserver.table.TableDataServer.SendTableDataSchema
 import com.paulsnomura.mdserver.table.TableDataServer.SendEntireTableData
 import com.paulsnomura.mdserver.table.TableDataServer.SendTableDataRow
 import com.paulsnomura.mdserver.table.TableDataServer.UpdateTableDataSchma
 import com.paulsnomura.mdserver.table.TableDataServer.MessageCase
 import com.paulsnomura.mdserver.table.TableDataServer.Logger
-
 import akka.actor.Actor
 import akka.actor.actorRef2Scala
 
@@ -19,36 +18,11 @@ abstract class TableDataServer( pKeyName : String ) extends Actor {
 	var keyedItems : Map[String, TableDataRow] = Map[String, TableDataRow]() //current table content
 	var schema     : TableDataSchema = new TableDataSchema( List() )         //current table schema
 
-	type clientIdentifierType //typically a String
-	
-    //Dependency Injection
-    protected def publisher  : Publisher[TableDataTransmittable]  //Publisher publishes TableData
-    protected def subscriber : Subscriber[String]                 //Subscriber gets client names
-           
     def primaryKeyName = pKeyName
+    
+    def broadcast( data : TableDataTransmittable ) : Unit
 
-    def callback(recipientName: String) : Unit = {
-        self ! SendTableDataSchema(recipientName)
-        self ! SendEntireTableData(recipientName)
-    } 
-		
-	private def connect() : Unit = {
-	    publisher.connect()
-	    subscriber.connect()
-	}
-	
-	private def disConnect() : Unit = {
-	    publisher.disConnect()
-	    subscriber.disConnect()
-	}
-	
-    override def preStart(): Unit = {
-        connect() //both publisher and subscriber connect
-    } 
-
-    override def postStop(): Unit = {
-        disConnect() //both publisher and subscriber disconnect
-    }
+    def send( clientName: String, data : TableDataTransmittable ) : Unit
 
     //To enable compiler warning for non exhaustive match for MessageCase, 
     //Define this as a standalone pattern match rather than partial function on which compiler does not perform non exhaustive check
@@ -59,7 +33,7 @@ abstract class TableDataServer( pKeyName : String ) extends Actor {
                 case Some(primaryKey) => { //If the sent row has the primary key
                     val row = new TableDataRow(map)
                     
-                    publisher.broadcast(row)
+                    broadcast(row)
                     Logger.info( "{} broadcasted", row )
                     
                     keyedItems += (primaryKey.toString -> row)
@@ -67,14 +41,18 @@ abstract class TableDataServer( pKeyName : String ) extends Actor {
                 case None =>
                     Logger.warn( "{} does not contain the primary key column = {}", SendTableDataRow(map), primaryKeyName )
             }
-        }       	    
-        case SendEntireTableData(recipientName) => 
-            keyedItems.values.foreach( row => publisher.send(recipientName, row))
-        case SendTableDataSchema( recipientName ) => 
-            publisher.send(recipientName, schema)        
+        }
+        case ClientStartup(clientName) => {
+        	self ! SendTableDataSchema(clientName)
+        	self ! SendEntireTableData(clientName)
+        }            
+        case SendEntireTableData(clientName) => 
+            keyedItems.values.foreach( row => send(clientName, row))
+        case SendTableDataSchema( clientName ) => 
+            send(clientName, schema)        
         case UpdateTableDataSchma( additionalColumnNames ) => {
         	schema = new TableDataSchema( schema.getColumnNames.toList ++ additionalColumnNames )
-        	publisher.broadcast(schema)
+        	broadcast(schema)
         }
     }
     
@@ -86,7 +64,8 @@ object TableDataServer {
  
     sealed abstract class MessageCase
     case class  SendTableDataRow( map: Map[String, Any] ) extends MessageCase //sent by a listener to forward the table data row to server's clients
-    case class  SendTableDataSchema( recipientName: String ) extends MessageCase //1) @client's startup and 2) when the schema is updated
-    case class  SendEntireTableData( recipientName: String ) extends MessageCase //typically requested by a client on its startup
+    case class  ClientStartup( clientName : String )      extends MessageCase //when client starts up, it sends this message to the server
+    case class  SendTableDataSchema( clientName: String ) extends MessageCase //when client requests to send the schema 
+    case class  SendEntireTableData( clientName: String ) extends MessageCase //when client requests to send the entire data
     case class  UpdateTableDataSchma( additionalColumnNames : List[String] ) extends MessageCase 
 }
--- mdserver/src/test/scala/com/paulsnomura/mdserver/table/TableDataServerTest.scala
+++ mdserver/src/test/scala/com/paulsnomura/mdserver/table/TableDataServerTest.scala
@@ -1,19 +1,17 @@
 package com.paulsnomura.mdserver.table
 
 import scala.concurrent.duration.DurationInt
-
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.scalatest.FlatSpecLike
-
 import com.paulsnomura.mdserver.Publisher
 import com.paulsnomura.mdserver.Subscriber
 import com.paulsnomura.mdserver.table.TableDataServer.MessageCase
+import com.paulsnomura.mdserver.table.TableDataServer.ClientStartup
 import com.paulsnomura.mdserver.table.TableDataServer.SendEntireTableData
 import com.paulsnomura.mdserver.table.TableDataServer.SendTableDataRow
 import com.paulsnomura.mdserver.table.TableDataServer.SendTableDataSchema
 import com.paulsnomura.mdserver.table.TableDataServer.UpdateTableDataSchma
-
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
@@ -22,97 +20,123 @@ import akka.testkit.TestActorRef
 import akka.testkit.TestKit
 
 /**
- * testActor : when TableDataServerMock sends a message to itself, it relays the message to testActor for testing purpose
+ * testActor : when TableDataServerMock send() or broadcast() a message, it sends it back to testActor for testing
  */
-class TableDataServerMock( pub: Publisher[TableDataTransmittable], sub: Subscriber[String], testActor : ActorRef ) 
-extends TableDataServer( "Name" ){ //primary Key = "Name"
-    type clientIdentifierType = String
-    override def publisher = pub
-    override def subscriber = sub
-    
-    override def receive = {
-        case message : MessageCase => {
-            testActor ! message //relaying the message to testActor for testing purpose
-            super.receive( message )
-        } 
-    }    
+class TableDataServerMock( testActor : ActorRef ) extends TableDataServer("Name") { //primary Key = "Name"
+   
+    override def broadcast(data: TableDataTransmittable) =  
+        testActor ! ( "broadcast", data ) 
+        
+    override def send(clientName: String, data: TableDataTransmittable) = 
+        testActor ! ( "send", clientName, data )
 }
-
 
 /**
  * Be careful on testActor shared across test cases (i.e.) as the above TAbleDataServerMock relays messages back to testActor
  * messages from other test cases can be received in the current test case, which causes confusing errors
  */
-class TableDataServerTest 
+class TableDataServerTest
 extends TestKit(ActorSystem("TableDataServerTest")) with FlatSpecLike {
-    
-	"TableDataServer" should "establish connection on its (actor) startup" in {    
-	    val publisherMock  = mock(classOf[Publisher[TableDataTransmittable]])
-	    val subscriberMock = mock(classOf[Subscriber[String]])     
-	    val server = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock( publisherMock, subscriberMock, testActor ) ) ) 
-	    
-	    verify(publisherMock).connect()
-	    verify(subscriberMock).connect()	
-	}
-		
-	it should "receive & publish TableDataRow" in {
-	    val publisherMock  = mock(classOf[Publisher[TableDataTransmittable]])
-	    val subscriberMock = mock(classOf[Subscriber[String]])     
-	    val server = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock( publisherMock, subscriberMock, testActor ) ) ) 
 
-	    val message = new SendTableDataRow( Map[String, String]( "Name" -> "this is test" ) )
-	    server ! message 	
-	        
-	    //fishforMessage, since other messages like BroadcastTableDataSchema & GetAllRecordData can be relayed to testActor
-	    fishForMessage() { 
-	        case returnedMessage : SendTableDataRow => returnedMessage.map == message.map //if this condition is satisfied, immediate success
-	        case _ => false //fishForMessage keeps running while this returns false
-	    }
+    val sampleMap = Map[String, TableDataRow](
+        "Toyota" -> new TableDataRow(Map[String, Any]("Name" -> "Toyota", "Price" -> 100, "Volume" -> 50)),
+        "Honda"  -> new TableDataRow(Map[String, Any]("Name" -> "Honda",  "Price" -> 101, "Volume" -> 60)),
+        "Nissan" -> new TableDataRow(Map[String, Any]("Name" -> "Nissan", "Price" -> 102, "Volume" -> 70))
+    )
+    
+    val sampleSchema =  new TableDataSchema( List( "Name", "Price", "Volume" ) )  
+    
+    "TableDataServer" should "broadcast TableDataRow update" in {
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) ) 
+	    val message   = new SendTableDataRow( Map[String, String]( "Name" -> "this is test" ) )
+	    serverRef ! message 
 	    
-   	    verify(publisherMock).broadcast( new TableDataRow( message.map ) )
+	    expectMsg( 1.seconds , ( "broadcast", new TableDataRow( message.map ) ) )	 
 	}
 	
+    it should "broadcast schema update" in {
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) ) 	    
+	    serverRef ! UpdateTableDataSchma( sampleSchema.getColumnNames ) 
+	    	    
+	    expectMsg( 1.seconds, ( "broadcast", sampleSchema ) )	    
+	}
+    
+    it should "process UpdateTableDataSchema message appropriately" in {
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) )
+	    
+	    serverRef ! UpdateTableDataSchma( List( "Name", "Price", "Volume" ) )      
+	    expectMsg( 1.seconds, ( "broadcast", new TableDataSchema( List( "Name", "Price", "Volume" ) ) ) )
+	            
+	    serverRef ! UpdateTableDataSchma( List( "Name", "High", "Low" ) )      
+	    expectMsg( 1.seconds, ( "broadcast", new TableDataSchema( List( "Name", "Price", "Volume", "High", "Low" ) ) ) ) 
+	}
+	
+    it should "send current schema on request" in {
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) )
+	    val server    = serverRef.underlyingActor 	    
+	    server.schema = sampleSchema 
+
+	    val clientName = "myself";
+	    serverRef ! SendTableDataSchema( clientName ) 
+	    	    
+	    expectMsg( 1.seconds, ( "send", clientName, sampleSchema ) )	    
+	}
+
+    it should "send entire data on request" in {
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) )
+	    val server    = serverRef.underlyingActor
+	    server.keyedItems = sampleMap
+
+	    val clientName = "myself";
+	    serverRef ! SendEntireTableData( clientName ) 
+	    	    
+	    expectMsgAllOf( 1.seconds, 
+            ( "send", clientName, sampleMap.getOrElse("Toyota", throw new Exception("Toyota Not Found!!")) ),
+            ( "send", clientName, sampleMap.getOrElse("Honda",  throw new Exception("Honda Not Found!!"))  ),
+            ( "send", clientName, sampleMap.getOrElse("Nissan", throw new Exception("Nissan Not Found!!")) )
+        )
+	            //sampleMap.values.map( row => ("send", clientName, row) ) )	    
+	}
+
 	it should "send current schema and entire data to client on client's startup" in {
-	    val publisherMock  = mock(classOf[Publisher[TableDataTransmittable]])
-	    val subscriberMock = mock(classOf[Subscriber[String]])    
-	    
-	    val server = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock( publisherMock, subscriberMock, testActor ) ) ).underlyingActor 
-	    
-	    val clientID = "myself";
-	    server.callback( clientID )
-	    expectMsgAllOf( 1.seconds, SendEntireTableData( clientID ), SendTableDataSchema( clientID ) )
-	    
-//	    verify(publisherMock).send(clientID, Map[String, Any]())
-//	    verify(publisherMock).send(clientID, new TableDataSchema())
-	    
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) )
+	    val server    = serverRef.underlyingActor 	    
+	    server.schema = sampleSchema 
+	    server.keyedItems = sampleMap
+
+	    val clientName = "myself";
+	    serverRef ! ClientStartup( clientName )
+
+	    expectMsgAllOf( 1.seconds, 
+	        ( "send", clientName, sampleSchema ),
+            ( "send", clientName, sampleMap.getOrElse("Toyota", throw new Exception("Toyota Not Found!!")) ),
+            ( "send", clientName, sampleMap.getOrElse("Honda",  throw new Exception("Honda Not Found!!"))  ),
+            ( "send", clientName, sampleMap.getOrElse("Nissan", throw new Exception("Nissan Not Found!!")) )
+        )
 	}
 	
 	it should "construct the inner table consistent with cumulative SendTableDataRow inputs" in {
-	    val publisherMock  = mock(classOf[Publisher[TableDataTransmittable]])
-	    val subscriberMock = mock(classOf[Subscriber[String]])     
-	    val server = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock( publisherMock, subscriberMock, testActor ) ) )
+	    val serverRef = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock(testActor) ) )
+	    val server    = serverRef.underlyingActor 	    
+	    server.schema = sampleSchema 
 
-        val expectedMap = Map[String, TableDataRow](
-            "Toyota" -> new TableDataRow(Map[String, Any]("Name" -> "Toyota", "Price" -> 100, "Volume" -> 50)),
-            "Honda" ->  new TableDataRow(Map[String, Any]("Name" -> "Honda",  "Price" -> 101, "Volume" -> 60)),
-            "Nissan" -> new TableDataRow(Map[String, Any]("Name" -> "Nissan", "Price" -> 102, "Volume" -> 70))
-        )
-	    
-	    expectedMap.values.foreach( x => server.underlyingActor.receive( new SendTableDataRow( x.map ) ) )
-	    
-	    assert( server.underlyingActor.keyedItems == expectedMap )
-	}
-	
-	it should "process UpdateTableDataSchema message appropriately" in {
-	    val publisherMock  = mock(classOf[Publisher[TableDataTransmittable]])
-	    val subscriberMock = mock(classOf[Subscriber[String]])     
-	    val server = TestActorRef[TableDataServerMock]( Props( new TableDataServerMock( publisherMock, subscriberMock, testActor ) ) )
-	    
-	    server ! UpdateTableDataSchma( List( "Name", "Price", "Volume" ) )    
-	    assert( server.underlyingActor.schema == new TableDataSchema( List( "Name", "Price", "Volume" ) ) )
+	    assert( 0 == server.keyedItems.size )
 
-	    server ! UpdateTableDataSchma( List( "Name", "High", "Low" ) )    
-	    assert( server.underlyingActor.schema == new TableDataSchema( List( "Name", "Price", "Volume", "High", "Low" ) ) )
+	    sampleMap.values.foreach( x => server.processMessage( new SendTableDataRow( x.map ) ) )
+	    assert( sampleMap == server.keyedItems )
+	 
+	    server.processMessage( new SendTableDataRow( Map[String, Any]("Name" -> "Toyota", "Price" -> 200, "Volume" -> 50) ) )
+	    server.processMessage( new SendTableDataRow( Map[String, Any]("Name" -> "Suzuki", "Price" -> 100, "Volume" -> 50) ) )
+	    
+	     val expectedMap = Map[String, TableDataRow](
+	        "Toyota" -> new TableDataRow(Map[String, Any]("Name" -> "Toyota", "Price" -> 200, "Volume" -> 50)),
+	        "Honda"  -> new TableDataRow(Map[String, Any]("Name" -> "Honda",  "Price" -> 101, "Volume" -> 60)),
+	        "Nissan" -> new TableDataRow(Map[String, Any]("Name" -> "Nissan", "Price" -> 102, "Volume" -> 70)),
+	        "Suzuki" -> new TableDataRow(Map[String, Any]("Name" -> "Suzuki", "Price" -> 100, "Volume" -> 50))
+	    )
+	    
+	    assert( expectedMap == server.keyedItems )
 	}
+
 	
 }
