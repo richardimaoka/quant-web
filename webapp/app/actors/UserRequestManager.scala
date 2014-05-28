ebe56039da705740c694690784803872ddb8b8c9
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2014-05-29 00:56:23 +0900

    more log messages on UserRequestManager

--- webapp/app/actors/UserRequestManager.scala
+++ webapp/app/actors/UserRequestManager.scala
@@ -8,12 +8,13 @@ import UserRequestManager._
 import utils.{ServiceManagerTrait, ServiceManager}
 import com.quantweb.mdserver.table.TableDataServer.ClientStartup
 import org.apache.logging.log4j.LogManager
+
 /**
  * Created by nishyu on 2014/05/26.
  */
 class UserRequestManager extends Actor {
   val logger = LogManager.getLogger(this.getClass().getName())
-  val serviceManager : ServiceManagerTrait = ServiceManager
+  val serviceManager: ServiceManagerTrait = ServiceManager
 
   case class UserChannel(enumerator: Enumerator[JsValue], channel: Channel[JsValue])
 
@@ -22,24 +23,30 @@ class UserRequestManager extends Actor {
   def registerUser(username: String): Unit = {
     userMap.get(username) match {
       //If username is found, the do nothing
-      case Some(_)  =>
-        logger.info( "username = '{}' already exists", username )
+      case Some(_) =>
+        logger.info("Username = '{}' already exists", username)
 
       //If username is not found in userMap, add it
       case None => {
+        logger.info("Username = '{}' does not exist - registering the user", username)
+
         val (enumerator, channel) = Concurrent.broadcast[JsValue]
         userMap += (username -> UserChannel(enumerator, channel))
 
+        logger.info("Username = '{}' registered", username)
+
         val toUserActor = context.actorOf(Props(new WebSocketActorToUser(channel)), username)
         serviceManager.marketDataTableService ! ClientStartup(toUserActor)
+
+        logger.info("Forwarding ActorRef for username = '{}' to services ", username)
       }
     }
   }
 
   override def receive = {
     case RegisterUser(username) => registerUser(username)
-    case GetUserEnumerator(username)   => userMap.get(username) map (userChannel => sender ! userChannel.enumerator)
-    case GetUserIteratee(username)     => userMap.get(username) map (userChannel => sender ! userChannel.enumerator)
+    case GetUserEnumerator(username) => userMap.get(username) map (userChannel => sender ! userChannel.enumerator)
+    case GetUserIteratee(username) => userMap.get(username) map (userChannel => sender ! userChannel.enumerator)
   }
 }
 
@@ -47,7 +54,6 @@ object UserRequestManager {
 
   sealed abstract class UserRequestCase
   case class RegisterUser(username: String) extends UserRequestCase
-  case class GetUserEnumerator(username: String)   extends UserRequestCase
-  case class GetUserIteratee(username: String)     extends UserRequestCase
-
+  case class GetUserEnumerator(username: String) extends UserRequestCase
+  case class GetUserIteratee(username: String) extends UserRequestCase
 }
