38dcd0d00262aa9f95c97a34444e40ba023204d5
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2014-06-14 10:06:04 +0900

    Order model

--- core/src/main/scala/com/quantweb/order/Order.scala
+++ core/src/main/scala/com/quantweb/order/Order.scala
@@ -1,8 +1,15 @@
 package com.quantweb.order
 
+import org.joda.time.DateTime
+
 /**
  * Created by nishyu on 2014/06/08.
  */
-class Order {
-
-}
+case class Order(
+  assetName: String,
+  price: Double,
+  quantity: Double,
+  buySell: String,
+  id: String,
+  timeCreated: DateTime
+)
